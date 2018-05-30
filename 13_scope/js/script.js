b6b0d3531c399ab732f8fa71f6aaa35fd9c6a3f2
vladis-study <vladis-study@users.noreply.github.com>
2018-05-30 23:05:14 +0300

    add scope lesson

---
+++ 13_scope/js/script.js
@@ -0,0 +1,26 @@
+console.log('You are at ' + window.location);
+
+var a = 'Hi';
+
+function myFunc() {
+    a = 'hello';
+    return a;
+}
+
+console.log(a);
+
+function myFunc2() {
+    var b = 3;
+    console.log(b);
+}
+
+myFunc2();
+
+
+if (true){
+    var c = 5;
+    let d = 6;
+}
+
+console.log(c);
+console.log(d); // ошибка
